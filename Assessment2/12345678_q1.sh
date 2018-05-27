e519c02964a087bd6dad00afe1538876707709d5
pushan-mukerjee <pushan.mukerjee@gmail.com>
2018-05-27 22:38:22 +1000

    ass 2

---
+++ Assessment2/12345678_q1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Student Full Name"
+echo "Student Number"
+
+echo "1. Create a folder on the HDFS and name it after your student number: /tmp/<student_number>"
+# write your code here (uncomment the line!). For example:
+pwd
+# or
+ls -la
+#...
+# add comments using echo command: 
+echo "An example of irrelevant comment!"
+
+echo "2. List all the files in that folder."
+#...
+echo "3. Transfer the above file to the HDFS folder that you just created (use absolute paths)."
+#...
+echo "4. Print 10 lines (with any preferred order) of the the transferred file which is now stored on HDFS."
+#...
+echo "
+The End!
+"
